b684c1d80a4f5e1f82a2cdf524265c527cea2128
Axicer <julienrabez@gmail.com>
2017-10-01 15:38:16 +0200

    Added simple throwError functions

--- cgstandardlib/src/main/java/fr/creativegames/cgstandardlib/exceptions/ErrorHandler.java
+++ cgstandardlib/src/main/java/fr/creativegames/cgstandardlib/exceptions/ErrorHandler.java
@@ -1,5 +1,15 @@
 package fr.creativegames.cgstandardlib.exceptions;
 
+import fr.creativegames.cgstandardlib.exceptions.errors.AbstractError;
+
+import java.io.PrintStream;
+
 public class ErrorHandler {
-	
+
+    public static void throwError(AbstractError error, PrintStream stream){
+        stream.print("["+error.getLevel().name()+"] "+error.getName() + " > "+error.getDescription());
+    }
+    public static void throwError(AbstractError error){
+        throwError(error, System.out);
+    }
 }
